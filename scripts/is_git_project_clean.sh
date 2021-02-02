437b5b849c6219157a24e4e3c360adc816234055
Chris Butler <chris@thebutlers.me>
2021-02-03 07:29:31 +1100

    Added script to test whether git directory is dirty or not.
    Signed-off-by: Chris Butler <chris@thebutlers.me>

---
+++ scripts/is_git_project_clean.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+if [ -z "$(git status --porcelain)" ]; then 
+  # Working directory clean
+  echo "git working directory is clean"
+  exit 0
+else 
+  # Uncommitted changes
+  echo "git working directory is dirty,"
+  echo "Files causing issues:"
+  echo ""
+  echo "$(git status --porcelain)"
+  echo ""
+  exit 1
+fi
